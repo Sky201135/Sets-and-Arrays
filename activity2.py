c465947bbb76e01acd41799453053e0159daf693
Sky201135 <kazisakiya11@outlook.com>
2026-02-21 22:18:59 +0600

    smth

---
+++ activity2.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original sets elements:")
+print("Set x:", setx)
+print("Set y:", sety)
+print("\nIntersection of set x and set y:")
+setz = setx.intersection(sety)
+print(setz)
